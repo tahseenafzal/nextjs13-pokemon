a4e10cefab5e729444c88d0ec636ab1a167b13e0
Tehseen Afzal <51958490+tahseenafzal@users.noreply.github.com>
2023-03-16 16:43:33 +0500

    Create types.ts

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export interface Pokemon {
+  id: number;
+  name: string;
+  type: string;
+  hp: number;
+  attack: number;
+  defence: number;
+  special_attack: number;
+  special_defence: number;
+  speed: number;
+}
